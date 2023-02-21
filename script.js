00747ca370e76046ebf31ee1e13231236b76ba3c
tzutzuliu <youngya0207@gmail.com>
2023-02-21 20:53:06 +0800

    update 一個可以做筆記的暫存檔

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.querySelector(".web");
+// Scene
+const scene = new THREE.Scene();
+// Object (red cube)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "blue" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene,camera)
